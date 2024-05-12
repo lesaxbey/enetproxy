1051886fbbb6cc28ada591c6db2d9095a3b7309c
lesaxbey <167888916+lesaxbey@users.noreply.github.com>
2024-05-12 19:17:04 +0300

    Update server.h

--- proxy/server.h
+++ proxy/server.h
@@ -21,7 +21,7 @@ class server {
     int m_token = 0;
     std::string m_server = "213.179.209.168";
     int m_port = 17198;
-    int m_proxyport = 17191;
+    int m_proxyport = 17091;
     world m_world;
     bool start();
     void quit();
